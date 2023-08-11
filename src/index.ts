a66358f5b2ea47995618b2924c8327cfecd7483e
tinpk <phamkientin@gmail.com>
2023-08-11 09:55:50 +0700

    Delete file bundle

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ class Index {
   constructor() {
     this.server = new Server();
   }
-
   public madeSever() {
     this.server.createServer();
   }
 }
 
 new Index().madeSever()
+
+// Hàng đợi tin nhắn (RabbitMQ)
